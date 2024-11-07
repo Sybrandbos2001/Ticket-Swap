54612a458b8e27e4ceed184b29d4d8ef263188af
Sybrand Bos <snjw.bos@student.avans.nl>
2024-11-07 11:53:14 +0100

    Initial commit

---
+++ apps/ticket-swap-api/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'ticket-swap-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/ticket-swap-api',
+};
